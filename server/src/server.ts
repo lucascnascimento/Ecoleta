5286ecddd45bc4461b8fc7515d304767b847bde3
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-01 09:33:20 -0300

    Criação do projeto do server

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/users", (request, response) => {
+  console.log("Listagem de usuários");
+
+  response.json(["Diego", "Cleiton", "Lucas", "Daniel"]);
+});
+
+app.listen(3333);
