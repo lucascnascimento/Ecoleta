8258a500573597194c63f226b8d52bdd0bc54f46
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-02 21:03:28 -0300

    Adição do cors e do uso de arquivo estáticos pelo express

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import cors from "cors";
+import path from "path";
 import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json()); // P/ o express entender o corpo da requisição em formato json
 app.use(routes);
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);
