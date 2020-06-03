c2e698edaa985218c1b0517cc38dda40864a4a22
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-02 21:02:07 -0300

    Refatoração e componentização do arquivo de rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,21 @@
 import express from "express";
 
-const routes = express.Router();
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
+//index, show, create, update, delete
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get("/items", itemsController.index);
+
+routes.post("/points", pointsController.create);
+routes.get("/points", pointsController.index);
+routes.get("/points/:id", pointsController.show);
 
 export default routes;
+
+// Service Pattern
+// Repository Pattern
