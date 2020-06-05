c16a8c7d2dab6652048317c067402982587808a0
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-06-05 10:09:53 -0300

    Drpzone e upload de dados no formdata

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -6,6 +6,8 @@ import axios from "axios";
 import { LeafletMouseEvent, LeafletEvent } from "leaflet";
 import api from "../../services/api";
 
+import Dropzone from "../../components/Dropzone";
+
 import "./styles.css";
 
 import logo from "../../assets/logo.svg";
@@ -51,6 +53,7 @@ const CreatePoint: React.FC = () => {
     0,
   ]);
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -144,16 +147,20 @@ const CreatePoint: React.FC = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+
+    if (selectedFile) {
+      data.append("image", selectedFile);
+    }
 
     await api.post("points", data);
 
@@ -177,6 +184,8 @@ const CreatePoint: React.FC = () => {
         <h1>
           Cadastro do <br /> do ponto de coleta
         </h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
